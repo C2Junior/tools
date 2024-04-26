b1d7d98c5ca7697f80967ad9a9d2169cf7a54ec1
Carlos A S Cunha Jr <117955370+C2Junior@users.noreply.github.com>
2024-04-26 20:06:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ document.getElementById("combinationForm").addEventListener("submit", function(e
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "";
 
+    // Calcular o número total de combinações possíveis
+    var totalComb = totalCombinations(n, m);
+    var totalCombDiv = document.getElementById("totalCombinations");
+    totalCombDiv.textContent = "Número total de combinações possíveis: " + totalComb;
+
     for (var i = 0; i < x; i++) {
         var randomCombination = getRandomCombination(elements, m);
         var combinationString = randomCombination.join(", ");
@@ -22,4 +27,21 @@ document.getElementById("combinationForm").addEventListener("submit", function(e
 function getRandomCombination(elements, m) {
     var shuffledElements = elements.sort(() => Math.random() - 0.5);
     return shuffledElements.slice(0, m).sort((a, b) => a - b);
+}
+
+function totalCombinations(n, m) {
+    // Calcular o número total de combinações possíveis
+    var numerator = factorial(n);
+    var denominator = factorial(m) * factorial(n - m);
+    return numerator / denominator;
+}
+
+function factorial(num) {
+    if (num === 0 || num === 1) {
+        return 1;
+    }
+    for (var i = num - 1; i >= 1; i--) {
+        num *= i;
+    }
+    return num;
 }
